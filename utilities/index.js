37aa1019b01b7dc18760f1e903b081df6a19db28
heathergibb <hrgibb@gmail.com>
2024-10-04 00:29:27 -0600

    assignment 3 functional

--- utilities/index.js
+++ utilities/index.js
@@ -31,30 +31,48 @@ Util.getNav = async function (req, res, next) {
 Util.buildClassificationGrid = async function(data){
     let grid
     if(data.length > 0){
-      grid = '<ul id="inv-display">'
+      grid = `<ul id="inv-display">`
       data.forEach(vehicle => { 
-        grid += '<li>'
-        grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-        + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-        + 'details"><img src="' + vehicle.inv_thumbnail 
-        +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-        +' on CSE Motors" /></a>'
-        grid += '<div class="namePrice">'
-        grid += '<hr />'
-        grid += '<h2>'
-        grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-        + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-        + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
-        grid += '</h2>'
-        grid += '<span>$' 
-        + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
-        grid += '</div>'
-        grid += '</li>'
+        grid += `<li class="car-card">`
+        grid += `<div class="image-container">`
+        grid += `<a href="../../inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details">`
+        grid += `<img src="${vehicle.inv_thumbnail}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model} on CSE Motors">`
+        grid += `</a></div>`
+        grid += `<div class="namePrice">`
+        grid += `<hr>`
+        grid += `<h2 class="car-card-heading">`
+        grid += `<a href="../../inv/detail/${vehicle.inv_id}" title="View ` 
+        grid += `${vehicle.inv_make} ${vehicle.inv_model} details">` 
+        grid += `${vehicle.inv_make} ${vehicle.inv_model}</a>`
+        grid += `</h2>`
+        grid += `<span>$${new Intl.NumberFormat(`en-US`).format(vehicle.inv_price)}</span>`
+        grid += `</div>`
+        grid += `</li>`
       })
-      grid += '</ul>'
+      grid += `</ul>`
     } else { 
-      grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+      grid += `<p class="notice">Sorry, no matching vehicles could be found.</p>`
     }
+    return grid
+  }
+
+  Util.buildInventoryDetailGrid = async function(data) {
+    let grid
+    grid = `<div id=details-container>`
+    grid += `<div class="image-container">`
+    grid += `<img src="${data.inv_image}" alt="Image of ${data.inv_make} ${data.inv_model} on CSE Motors" id="vehicle-img-large">` 
+    grid += `</div>`
+    grid += `<section id="vehicle-details">`
+    grid += `<h2 class="section-title">${data.inv_make} ${data.inv_model} Details</h2>`
+    grid += `<table><tbody>`
+    grid += `<tr><td><span class="details-label">Price: $${new Intl.NumberFormat('en-US').format(data.inv_price)}</span></td></tr>`
+    grid += `<tr><td><span class="details-label">Description: </span>`
+    grid += `<span class="details-text">${data.inv_description}</span></td></tr>`
+    grid += `<tr><td><span class="details-label">Color: </span>`
+    grid += `<span class="details-text">${data.inv_color}</span></td></tr>`
+    grid += `<tr><td><span class="details-label">Miles: </span>`
+    grid += `<span class="details-text">${new Intl.NumberFormat('en-US').format(data.inv_miles)}</span></td></tr>`
+    grid += `</tbody></table></section></div>`
     return grid
   }
 
