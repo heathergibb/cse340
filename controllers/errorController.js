c4ff36b9258d1d7ff3ec5a1146a0a1a96842a139
heathergibb <hrgibb@gmail.com>
2024-10-04 16:42:20 -0600

    fixed some responsive formatting issues

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,5 @@
 exports.throwError = (req, res, next) => {
-    const error = new Error("THIS IS A TEST!")
+    const error = new Error("ERROR: This is a test!")
     error.status = 500
     throw error
 }
