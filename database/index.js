3fd26c26af5f48838777fce81c5824cc518756f9
Heather Gibb <130530562+heathergibb@users.noreply.github.com>
2024-10-04 15:10:33 -0600

    Update index.js

--- database/index.js
+++ database/index.js
@@ -32,9 +32,6 @@ if (process.env.NODE_ENV == "development") {
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: true,
-    },
   })
   module.exports = pool
 }
